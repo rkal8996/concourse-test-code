73764c6c2ae6920f457e1200b3a2cb408d633bd1
Ravindu Shehan <ravindu.kaluhannadige@sysco.com>
2024-03-31 16:16:38 +0530

    Update

---
+++ simple-tasks/generate-files.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir generated-files
+echo "file1" > generated-files/file1
+echo "file2" > generated-files/file2
+echo "file3" > generated-files/file3
+echo "file4" > generated-files/file4
+
+ls generated-files/*
